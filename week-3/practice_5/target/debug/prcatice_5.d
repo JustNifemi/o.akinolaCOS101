4d1aa2fbf4beea2893206ee4b6557a61b2225d1f
JustNifemi <akinolaoluwanifemi06@gmail.com>
2024-10-28 13:30:48 +0100

    (My_third_commit)

---
+++ week-3/practice_5/target/debug/prcatice_5.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\o.akinolaCOS101\week-3\practice_5\target\debug\prcatice_5.exe: C:\Users\USER\Documents\o.akinolaCOS101\week-3\practice_5\src\main.rs
---
+++ week-3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let intrest:f32 = 8.0;
+    println!("intrest is {}",intrest );
+}
