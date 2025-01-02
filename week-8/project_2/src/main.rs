e5b586a1962e13da40d3bacef71a71f03bd50b70
JustNifemi <akinolaoluwanifemi06@gmail.com>
2025-01-02 09:37:38 +0100

    (In progress)

--- week-8/project_2/src/main.rs
+++ week-8/project_2/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::io;
+
+fn main(){
+
+    let student_names = vec!["Oluchi Mordi\n", "Adams Aliyu\n", "Shania Bolade\n", "Adekunle Gold\n", "Blanca Edemoh\n"];
+    let matric_no = vec!["ACC10211111\n", "ECO10110101\n", "CSC10328828\n", "EEE11020202\n", "MEE10202001\n"];
+    let department = vec!["Accounting\n", "Economics\n", "Computer\n", "Electrical\n", "Mechanical\n"];
+    let level = vec!["300\n","100\n","200\n","200\n","100\n"];
+
+
+    let mut sims_file = std::fs::File::create("PAU-SIMS.xls").expect("create failed");
+    sims_file.write_all("PAU SIMS\n".as_bytes()).expect("Failed to write");
+
+    sims_file.write_all("Student Name\n".as_bytes()).expect("Failed to write");
+    for std in student_names{
+        sims_file.write_all(std.as_bytes()).expect("Failed to write");
+    }
+
+    sims_file.write_all("Matric. Number\n".as_bytes()).expect("Failed to write");
+    for mtno in matric_no{
+        sims_file.write_all(mtno.as_bytes()).expect("Failed to write");
+    }
+
+    sims_file.write_all("Department\n".as_bytes()).expect("Failed to write");
+    for dpt in department{
+        sims_file.write_all(dpt.as_bytes()).expect("Failed to write");
+    }
+
+    sims_file.write_all("Level\n".as_bytes()).expect("Failed to write");
+    for lvl in level{
+        sims_file.write_all(lvl.as_bytes()).expect("Failed to write");
+    }
+
+    print!("File created.");
 }
--- week-8/project_3/src/main.rs
+++ week-8/project_3/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::Write;
+use std::io::Read;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+//reading the existing datasets
+    let mut file1 = std::fs::File::open("Commisioner Names.txt").unwrap();
+    let mut contents1 = String::new();
+    file1.read_to_string(&mut contents1).unwrap();
+
+
+    let mut file2 = std::fs::File::open("Geopolitical Zones.txt").unwrap();
+    let mut contents2 = String::new();
+    file2.read_to_string(&mut contents2).unwrap();
+
+
+    let mut file3 = std::fs::File::open("Ministry.txt").unwrap();
+    let mut contents3 = String::new();
+    file3.read_to_string(&mut contents3).unwrap();
 }
