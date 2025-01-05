96bf48bfc065e49a9731ac4ac14653b5c12d8951
JustNifemi <akinolaoluwanifemi06@gmail.com>
2025-01-05 13:53:58 +0100

    (Update...)

--- week-8/project_2/src/main.rs
+++ week-8/project_2/src/main.rs
@@ -2,15 +2,58 @@ use std::io::Write;
 use std::io;
 
 fn main(){
+    println!("PAU Student Management Information System (PAU-SMIS)");
 
-    let student_names = vec!["Oluchi Mordi\n", "Adams Aliyu\n", "Shania Bolade\n", "Adekunle Gold\n", "Blanca Edemoh\n"];
-    let matric_no = vec!["ACC10211111\n", "ECO10110101\n", "CSC10328828\n", "EEE11020202\n", "MEE10202001\n"];
-    let department = vec!["Accounting\n", "Economics\n", "Computer\n", "Electrical\n", "Mechanical\n"];
-    let level = vec!["300\n","100\n","200\n","200\n","100\n"];
+    let mut student_names:Vec<String> = Vec::new();
+    let mut matric_no:Vec<String> = Vec::new();
+    let mut department:Vec<String> = Vec::new();
+    let mut level:Vec<String> = Vec::new();
 
 
-    let mut sims_file = std::fs::File::create("PAU-SIMS.xls").expect("create failed");
-    sims_file.write_all("PAU SIMS\n".as_bytes()).expect("Failed to write");
+println!("Hello! Welcome to the PAU-SMIS input program.");
+
+
+// to determine the range for the loop
+println!("How many student entries would you like to make?");
+let mut num = String::new();
+io::stdin().read_line(&mut num).expect("Failed to read input");
+let num:u32 = num.trim().parse().expect("Failed to read input");
+
+
+// loop for input
+for x in 0..num{
+    println!("Enter student's name:");
+    let mut std_name = String::new();
+    io::stdin().read_line(&mut std_name).expect("Failed to read input");
+    let std_name:String = std_name.trim().parse().expect("Failed to read input");
+
+    println!("Enter student's matriculation number:");
+    let mut std_matric = String::new();
+    io::stdin().read_line(&mut std_matric).expect("Failed to read input");
+    let std_matric:String = std_matric.trim().parse().expect("Failed to read input");
+
+    println!("Enter student's department:");
+    let mut std_dep = String::new();
+    io::stdin().read_line(&mut std_dep).expect("Failed to read input");
+    let std_dep:String = std_dep.trim().parse().expect("Failed to read input");
+
+    println!("Enter student's level:");
+    let mut std_level = String::new();
+    io::stdin().read_line(&mut std_level).expect("Failed to read input");
+    let std_level:String = std_level.trim().parse().expect("Failed to read input");
+
+
+    //to push to the vectors
+    student_names.push(std_name + "\n");
+    matric_no.push(std_matric + "\n");
+    department.push(std_dep + "\n");
+    level.push(std_level + "\n")
+
+}
+
+
+    let mut sims_file = std::fs::File::create("PAU-SMIS.csv").expect("create failed");
+    sims_file.write_all("PAU SMIS\n".as_bytes()).expect("Failed to write");
 
     sims_file.write_all("Student Name\n".as_bytes()).expect("Failed to write");
     for std in student_names{
@@ -33,4 +76,5 @@ fn main(){
     }
 
     print!("File created.");
+    println!("Thank you for using the PAU-SMIS input program.");
 }
--- week-8/project_3/src/main.rs
+++ week-8/project_3/src/main.rs
@@ -4,6 +4,10 @@ use std::io;
 
 fn main() {
 
+    println!("Hello!");
+    println!("Merging datasets 'Commisioner Names.txt', 'Ministry.txt', 'Geopolitical Zones.txt' into 'Convicted Ministers.txt'");
+    println!("Please wait...");
+
 //reading the existing datasets
     let mut file1 = std::fs::File::open("Commisioner Names.txt").unwrap();
     let mut contents1 = String::new();
@@ -18,4 +22,14 @@ fn main() {
     let mut file3 = std::fs::File::open("Ministry.txt").unwrap();
     let mut contents3 = String::new();
     file3.read_to_string(&mut contents3).unwrap();
+
+    //creating a new file & writing the others into it
+
+    let mut new_file = std::fs::File::create("Convicted Ministers.txt").unwrap();
+    new_file.write_all(contents1.as_bytes()).expect("Failed to write");
+    new_file.write_all(contents3.as_bytes()).expect("Failed to write");
+    new_file.write_all(contents2.as_bytes()).expect("Failed to write");
+
+
+    println!("Process Successful!");
 }
